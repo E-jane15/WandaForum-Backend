eb13b7a4b1c88829b5cee93d8901f7a56ef972a7
Angelazango <angelazango2@gmail.com>
2025-01-22 06:03:14 +0100

    notification

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { DatabaseModule } from './database/database.module';
-import { UsersModule } from './users/users.module';
-import { JwtModule } from '@nestjs/jwt';
+import { NotificationsModule } from './notifications/notifications.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [UsersModule, DatabaseModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [NotificationsModule],
+  providers: [PrismaService],
 })
 export class AppModule {}
